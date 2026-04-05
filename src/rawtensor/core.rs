85864539bb8c0b7c991b91480005d3df61a8c3c5
lucailie11 <lucailie110@gmail.com>
2026-04-05 12:13:58 +0200

    derived clone

--- src/rawtensor/core.rs
+++ src/rawtensor/core.rs
@@ -2,7 +2,7 @@ use std::fmt;
 
 // Core RawTensor type: a pair of shape and data (stored in row-major order).
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct RawTensor {
     pub(super) shape: Box<[usize]>,
     pub(super) data: Box<[f64]>,
