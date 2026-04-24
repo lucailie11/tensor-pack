0413ef4937c1a411f439f607bbe5634aaf6f937b
lucailie11 <lucailie110@gmail.com>
2026-04-24 13:51:02 +0200

    elemenwise ops allow new-stride format, assigns are not inplace anymore

--- src/grad/binary.rs
+++ src/grad/binary.rs
@@ -3,10 +3,10 @@ use crate::Tensor;
 pub fn add_tensor_backprop(out: &Tensor, a: &Tensor, b: &Tensor) {
     if let Some(out_grad) = out.grad.borrow().as_ref() {
         if let Some(a_grad) = a.grad.borrow_mut().as_mut() {
-            *a_grad += out_grad;
+            a_grad.accumulate_1(out_grad, |g| g);
         }
         if let Some(b_grad) = b.grad.borrow_mut().as_mut() {
-            *b_grad += out_grad;
+            b_grad.accumulate_1(out_grad, |g| g);
         }
     }
 }
@@ -14,20 +14,21 @@ pub fn add_tensor_backprop(out: &Tensor, a: &Tensor, b: &Tensor) {
 pub fn sub_tensor_backprop(out: &Tensor, a: &Tensor, b: &Tensor) {
     if let Some(out_grad) = out.grad.borrow().as_ref() {
         if let Some(a_grad) = a.grad.borrow_mut().as_mut() {
-            *a_grad += out_grad;
+            a_grad.accumulate_1(out_grad, |g| g);
         }
         if let Some(b_grad) = b.grad.borrow_mut().as_mut() {
-            *b_grad -= out_grad;
+            b_grad.accumulate_1(out_grad, |g| -g);
         }
     }
 }
+
 pub fn mul_tensor_backprop(out: &Tensor, a: &Tensor, b: &Tensor) {
     if let Some(out_grad) = out.grad.borrow().as_ref() {
         if let Some(a_grad) = a.grad.borrow_mut().as_mut() {
-            *a_grad += &(out_grad * &b.raw);
+            a_grad.accumulate_2(out_grad, &b.raw, |g, b| g * b);
         }
         if let Some(b_grad) = b.grad.borrow_mut().as_mut() {
-            *b_grad += &(out_grad * &a.raw);
+            b_grad.accumulate_2(out_grad, &a.raw, |g, a| g * a);
         }
     }
 }
@@ -35,11 +36,10 @@ pub fn mul_tensor_backprop(out: &Tensor, a: &Tensor, b: &Tensor) {
 pub fn div_tensor_backprop(out: &Tensor, a: &Tensor, b: &Tensor) {
     if let Some(out_grad) = out.grad.borrow().as_ref() {
         if let Some(a_grad) = a.grad.borrow_mut().as_mut() {
-            *a_grad += &(out_grad / &b.raw);
+            a_grad.accumulate_2(out_grad, &b.raw, |g, b| g / b);
         }
         if let Some(b_grad) = b.grad.borrow_mut().as_mut() {
-            *b_grad -= &(out_grad * &(&a.raw / &(&b.raw * &b.raw)));
+            b_grad.accumulate_3(out_grad, &a.raw, &b.raw, |g, a, b| g * (-a) / (b * b));
         }
     }
 }
-
